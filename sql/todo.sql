3f97e3f1472b42fa9a81aae0abbfa82dbc6956e0
5imyboy <simonyong05@gmail.com>
2025-08-27 16:28:56 -0700

    initialize database

---
+++ sql/todo.sql
@@ -0,0 +1,14 @@
+drop database if exists todo;
+create database todo;
+use todo;
+
+drop table if exists task;
+
+create table task (
+	task_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title varchar(100) not null unique,
+    `description` varchar(1024) not null,
+	`status` ENUM('NOT_STARTED', 'IN_PROGRESS', 'COMPLETED'),
+    hours int not null default(0),
+    minutes int not null default(0)
+);
