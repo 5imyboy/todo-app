1cbe20334f55e93f6f3b2f0b14a222a69ebdc76f
5imyboy <simonyong05@gmail.com>
2026-05-01 11:50:51 -0700

    mobile: add logout button

--- client/todo-mobile/app/(tabs)/_layout.tsx
+++ client/todo-mobile/app/(tabs)/_layout.tsx
@@ -1,8 +1,15 @@
-import { Tabs } from "expo-router";
+import { Link, Tabs } from "expo-router";
+import { Button } from "react-native";
+
+const logoutButton = (
+  <Link href="/login" push asChild>
+    <Button title="Logout" />
+  </Link>
+);
 
 export default function TabLayout() {
   return (
-    <Tabs>
+    <Tabs screenOptions={{ headerRight: () => logoutButton }}>
       <Tabs.Screen name="not-started" options={{ title: "Not Started" }} />
       <Tabs.Screen name="in-progress" options={{ title: "In Progress" }} />
       <Tabs.Screen name="finished" options={{ title: "Finished" }} />
