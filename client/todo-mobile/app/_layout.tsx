e7284f122e73698950aeb5c3bab79c9822a0f6f0
5imyboy <simonyong05@gmail.com>
2026-05-03 14:36:48 -0700

    mobile: save user login

--- client/todo-mobile/app/_layout.tsx
+++ client/todo-mobile/app/_layout.tsx
@@ -1,6 +1,20 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import * as SecureStore from "expo-secure-store";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const router = useRouter();
+
+  useEffect(() => {
+    SecureStore.getItemAsync("token").then(token => {
+      if (token) {
+        router.replace("/(tabs)/not-started");
+      } else {
+        router.replace("/login");
+      }
+    });
+  }, []);
+
   return (
     <Stack>
       <Stack.Screen name="login" options={{ title: "Login" }} />
